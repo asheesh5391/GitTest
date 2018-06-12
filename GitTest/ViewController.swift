d032e84cb2dcc8a200ece8c3d5a1c8de6b4c03ce
Asheesh <asheesh@divya-mac.local>
2018-06-12 17:25:32 +0530

    Added Label to storyboard and showing revered string

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
     let message = "Hello Git!"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
         print(reverse(text: "stressed"))
+        self.messageLabel.text = reverse(text: "stressed")
     }
     
     func reverse(text: String) -> String {
